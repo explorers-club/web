a95dee169ef83afcc1d357da227ff5ded81d909e
Jon Mumm <jonathanrmumm@gmail.com>
2022-07-13 06:18:28 -0700

    r3f initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas, useLoader } from "@react-three/fiber";
+import { Suspense } from "react";
+import { RGBELoader } from "three-stdlib";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas
+        gl={{ physicallyCorrectLights: true }}
+        camera={{ position: [0, 0, 50] }}
+      >
+        <color attach="background" args={["#FFEECC"]} />
+        <Box />
+        <Suspense fallback={null}>
+        </Suspense>
+        {/* <ambientLight />
+        <pointLight position={[10, 10, 10]} /> */}
+      </Canvas>
     </div>
   );
 }
 
 export default App;
+
+const Box = () => {
+  // useLoader(RGBELoader, "./assets/envmap.hdr");
+
+  return (
+    <mesh>
+      <sphereGeometry args={[5, 10, 10]} />
+      <meshBasicMaterial color={0xff0000} />
+    </mesh>
+  );
+};
